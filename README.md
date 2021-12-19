f783d47cf9f11aaafa8ca6ef919c7934dd7e2b8d
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2021-12-19 12:18:10 +0100

    getting subtitles when present in the file

---
+++ README.md
@@ -0,0 +1,10 @@
+# QuickFlix
+
+- List all videos from hardrive, but only show mkv and mp4 in front
+- add them in the bdd
+- make a name shortener to keep only title and for tvshow season and episode
+- check audio codecs and reencode if necessary
+  - make a job that will not pressure too much the RPY, like 10 timeout after each, may be during the night
+- find a way to extract subtitles from videos when present
+  - check with ffprobe -v error -of json input.mkv -of json -show_entries "stream=index:stream_tags=language" -select_streams s
+    - it gives us a nice json file with the different languages
--- src/routes/ApiRouter/controller.js
+++ src/routes/ApiRouter/controller.js
@@ -1,12 +1,53 @@
 import fs, {access, constants} from 'fs'
 import path from 'path'
 import mime from 'mime'
+import ffmpeg from 'fluent-ffmpeg'
+import winston from 'winston/lib/winston/config';
+import { loggers } from 'winston';
+
+function extractSubtitleTrack(inputFile, streamInfo, outputFile) {
+  // console.log('=======================');
+  // console.log(inputFile);
+  // console.log('=======================');
+  // console.log(streamInfo);
+  // console.log('=======================');
+  // console.log(outputFile);
+  try {
+      const command = new ffmpeg(inputFile, { logger: console.debug } );
+          command
+          .on('start', function(command) {
+            console.log('Start: ', command);
+            console.log('=======================');
+          })
+          // .noAudio()
+          // .noVideo()
+          .outputOptions(
+            `-map 0:${streamInfo.index}`,
+            // '-c:s:0',
+            // 'webvtt'
+          )
+          .output(path.resolve('./public/' + outputFile))
+          .on('error', function(err, stdout, stderr) {
+              console.log('=======================');
+              console.log('An error occurred: ' + err.message, err, stderr);
+          })
+          .on('end', function() {
+              
+          });
+          command.run();
+
+  } catch (e) {
+      console.log(e);
+      console.log(e.code);
+      console.log(e.msg);
+  }
+
+}
 
 export default class ApiController {
   stream = async (req, res, next) => {
     const params = req.params;
     const movie = path.resolve("./public/" + req.params.filename);
-
 
     // Check if the file exists in the current directory.
     access(movie, constants.F_OK, (err) => {
@@ -19,24 +60,49 @@ export default class ApiController {
       if (!range) {
         return res.sendStatus(416);
       }
+
+      // Finding subtitles
+      ffmpeg.ffprobe(movie, (err, metadata) => {
+        const title = metadata.format.tags.title
+        const languages = {}
+        
+        metadata.streams.forEach((element) => {
+          if(element.codec_name.includes('subrip') ) {
+            const language = element.tags.language
+            let outputFile = ''
+
+            // Keeping track of how many subtitles we have for one language
+            if(languages[language] > 0) {
+              languages[language] += 1
+            }else {
+              languages[language] = 1
+            }
+            
+            outputFile = `${title}.${languages[language]}.${language}.vtt`
+            // console.log(languages, languages[language])
+
+            extractSubtitleTrack(movie, element, outputFile);
+          }
+        });
+      })
       
-      //Chunk logic here
+      // Chunk logic here
       var positions = range.replace(/bytes=/, "").split("-");
       var start = parseInt(positions[0], 10);
       var total = stats.size;
       var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
       var chunksize = end - start + 1;
-      console.log(mime.getType(req.params.filename))
+
       res.writeHead(206, {
         "Transfer-Encoding": "chunked",
 
         "Content-Range": "bytes " + start + "-" + end + "/" + total,
 
         "Accept-Ranges": "bytes",
-
+        // chunksize
         "Content-Length": chunksize,
-
-        "Content-Type": 'video/mp4',
+        //mime.getType(req.params.filename) 
+        "Content-Type": mime.getType(req.params.filename),
       });
 
       const stream = fs
