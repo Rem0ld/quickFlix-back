a4e7beb86e9af733ab324f149aa67fa2b7acb3c9
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-01-21 15:07:49 +0100

    update connection string

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-import Express from "express"
-import Server from "./src/config/server"
-import dotenv from "dotenv"
-import middlewares from "./src/config/middlewares"
-import routes from "./src/routes"
-import mongoose from "mongoose"
+import Express from "express";
+import Server from "./src/config/server";
+import dotenv from "dotenv";
+import middlewares from "./src/config/middlewares";
+import routes from "./src/routes";
+import mongoose from "mongoose";
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT
-const app = Express()
+const port = process.env.PORT;
+const app = Express();
 mongoose.connect(
   process.env.NODE_ENV === "development"
     ? process.env.DB_CONNECTION_STRING
-    : "mongodb://localhost:27017/db_quickFlix --username amdin --password 1234",
+    : "mongodb://amdin:1234@localhost:27017/db_quickFlix?authSource=admin",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
   error => {
-    console.log(error)
+    if (error) {
+      // TODO: log for db connection error
+      console.log(error);
+    }
   }
-)
-const server = new Server(app)
+);
+const server = new Server(app);
 
-server.middlewares(middlewares)
-server.routes(routes)
-server.start(port)
+server.middlewares(middlewares);
+server.routes(routes);
+server.start(port);
--- src/routes/DiscoverRouter/controller.js
+++ src/routes/DiscoverRouter/controller.js
@@ -1,28 +1,28 @@
-import path from "path"
-import { findFiles } from "../../utils/fileManipulation"
-import { videoModel } from "../../schemas/Video"
-import srt2vtt from "srt-to-vtt"
-import { createReadStream, createWriteStream, rm } from "fs"
-import { subtitleModel } from "../../schemas/Subtitles"
-import { tvShowModel } from "../../schemas/TvShow"
-import fetch from "node-fetch"
-import dotenv from "dotenv"
+import path from "path";
+import { findFiles } from "../../utils/fileManipulation";
+import { videoModel } from "../../schemas/Video";
+import srt2vtt from "srt-to-vtt";
+import { createReadStream, createWriteStream, rm } from "fs";
+import { subtitleModel } from "../../schemas/Subtitles";
+import { tvShowModel } from "../../schemas/TvShow";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const matchExt = /(\w+)$/i
-const matchLng = /(\w+).\w+$/
-const regexTvShow = /(s\d{1,2})(e\d{1,2})/i
-const regExBasename = /([ .\w']+?)($|mp3|[s|S]\d{1,}|\(.*\)|[A-Z]{2,}|\W\d{4}\W?.*)/
-const regexIsSubtitle = /(vtt|srt|sfv)/i
-const movieDbUrl = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.API_MOVIEDB}&page=1&include_adult=false&language=en-US&query=`
-const imageDbUrl = `https://image.tmdb.org/t/p/w500`
+const matchExt = /(\w+)$/i;
+const matchLng = /(\w+).\w+$/;
+const regexTvShow = /(s\d{1,2})(e\d{1,2})/i;
+const regExBasename = /([ .\w']+?)($|mp3|[s|S]\d{1,}|\(.*\)|[A-Z]{2,}|\W\d{4}\W?.*)/;
+const regexIsSubtitle = /(vtt|srt|sfv)/i;
+const movieDbUrl = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.API_MOVIEDB}&page=1&include_adult=false&language=en-US&query=`;
+const imageDbUrl = `https://image.tmdb.org/t/p/w500`;
 
 // Need to add id after tv/ then add the remaining of the URL
-const detailDbUrl = `https://api.themoviedb.org/3/` // tv | movie / {id}
-const apikeyTvShowUrl = `?api_key=${process.env.API_MOVIEDB}&language=en-US`
+const detailDbUrl = `https://api.themoviedb.org/3/`; // tv | movie / {id}
+const apikeyTvShowUrl = `?api_key=${process.env.API_MOVIEDB}&language=en-US`;
 
-const basePath = path.resolve("./public/")
+const basePath = path.resolve("./public/");
 const excludedExtension = [
   "DS_Store",
   "nfo",
@@ -35,61 +35,61 @@ const excludedExtension = [
   "jpg",
   "jpeg",
   "png",
-]
+];
 
 export default class DiscoverController {
   async discover(_, res) {
-    console.log("Starting discovering")
+    console.log("Starting discovering");
     let countCreatedVideo = 0,
       countCreatedTvShow = 0,
-      countUpdatedTvShow = 0
-    const subDirectories = []
-    const files = await findFiles(basePath)
+      countUpdatedTvShow = 0;
+    const subDirectories = [];
+    const files = await findFiles(basePath);
 
     const goThrough = async (files, extraPath = "") => {
       for (const file of files) {
-        const ext = path.extname(file.name)
+        const ext = path.extname(file.name);
 
-        const exclude = excludedExtension.some(ext => file.name.includes(ext))
+        const exclude = excludedExtension.some(ext => file.name.includes(ext));
 
-        if (exclude) continue
+        if (exclude) continue;
 
         if (file.isDirectory() || file.isSymbolicLink()) {
           //          console.log({file}, 'is directory')
-          const sub = await findFiles(basePath + "/" + extraPath + "/" + file.name)
+          const sub = await findFiles(basePath + "/" + extraPath + "/" + file.name);
           subDirectories.push({
             directory: extraPath + "/" + file.name,
             content: sub.filter(el => !el.name.includes("DS_Store")),
-          })
-          continue
+          });
+          continue;
         }
 
         // Here we need to work the file
-        const filename = path.basename(file.name, ext)
-        const absolutePath = path.resolve(basePath + "/" + extraPath + "/")
+        const filename = path.basename(file.name, ext);
+        const absolutePath = path.resolve(basePath + "/" + extraPath + "/");
 
         if (!regexIsSubtitle.test(ext)) {
           // It's a video we need to check if already exists in db first
-          let basename = filename.match(regExBasename)
+          let basename = filename.match(regExBasename);
 
           if (!basename) {
-            console.error("Something wrong has happened")
+            console.error("Something wrong has happened");
             // TODO: log here with filename
-            console.error(filename)
+            console.error(filename);
           }
 
-          basename = basename[1].replaceAll(".", " ").trim().toLowerCase()
+          basename = basename[1].replaceAll(".", " ").trim().toLowerCase();
 
-          const isTvShow = filename.match(regexTvShow)
-          const season = isTvShow && isTvShow[1].toLowerCase()
-          const episode = isTvShow && isTvShow[2].toLowerCase()
-          const name = isTvShow ? `${basename} ${season}${episode}` : basename
+          const isTvShow = filename.match(regexTvShow);
+          const season = isTvShow && isTvShow[1].toLowerCase();
+          const episode = isTvShow && isTvShow[2].toLowerCase();
+          const name = isTvShow ? `${basename} ${season}${episode}` : basename;
 
           // check if already exists
-          const existInDb = await videoModel.find({ name })
-          let video
+          const existInDb = await videoModel.find({ name });
+          let video;
           if (!existInDb.length) {
-            const location = path.resolve(absolutePath + "/" + file.name)
+            const location = path.resolve(absolutePath + "/" + file.name);
             video = await videoModel.create({
               filename,
               basename,
@@ -97,19 +97,19 @@ export default class DiscoverController {
               ext,
               location,
               type: isTvShow ? "tv" : "movie",
-            })
+            });
 
-            countCreatedVideo++
-            console.log("{video} created")
+            countCreatedVideo++;
+            console.log("{video} created");
             // TODO: log here which video has been created with name and location
           } else {
-            video = existInDb[0]
+            video = existInDb[0];
           }
 
           if (isTvShow) {
-            const seasonNumber = season.match(/(\d+)/)[0]
-            const episodeNumber = episode.match(/(\d+)/)[0]
-            let tvShow = await tvShowModel.findOne({ name: basename })
+            const seasonNumber = season.match(/(\d+)/)[0];
+            const episodeNumber = episode.match(/(\d+)/)[0];
+            let tvShow = await tvShowModel.findOne({ name: basename });
 
             if (!tvShow) {
               tvShow = await tvShowModel.create({
@@ -120,43 +120,43 @@ export default class DiscoverController {
                     episodes: [{ number: episodeNumber, ref: video._id }],
                   },
                 ],
-              })
-              console.log("tvshow created")
-              countCreatedTvShow++
+              });
+              console.log("tvshow created");
+              countCreatedTvShow++;
               // TODO: log here which tvShow has been created with name and location
             } else {
-              const { seasons } = tvShow
+              const { seasons } = tvShow;
               const seasonIsPresent = seasons.findIndex(el => {
-                return +el.number === +seasonNumber
-              })
+                return +el.number === +seasonNumber;
+              });
 
               if (seasonIsPresent === -1) {
                 seasons.push({
                   number: seasonNumber,
                   episodes: [{ number: episodeNumber, ref: video._id }],
-                })
-                tvShow.seasons = seasons
-                await tvShow.save()
-                console.log("tvshow updated - new season")
-                countUpdatedTvShow++
-                continue
+                });
+                tvShow.seasons = seasons;
+                await tvShow.save();
+                console.log("tvshow updated - new season");
+                countUpdatedTvShow++;
+                continue;
               } else {
-                const modifiedSeason = seasons.splice(seasonIsPresent, 1)
+                const modifiedSeason = seasons.splice(seasonIsPresent, 1);
                 const episodeIsPresent = modifiedSeason[0].episodes.findIndex(
                   el => +el.number === +episodeNumber
-                )
-                console.log(video.name, episodeIsPresent)
+                );
+                console.log(video.name, episodeIsPresent);
                 if (episodeIsPresent === -1) {
                   modifiedSeason[0].episodes.push({
                     number: episodeNumber,
                     ref: video._id,
-                  })
-                  seasons.push(modifiedSeason[0])
-                  tvShow.seasons = seasons
-                  await tvShow.save()
-                  console.log("tvshow updated - new episode")
-                  countUpdatedTvShow++
-                  continue
+                  });
+                  seasons.push(modifiedSeason[0]);
+                  tvShow.seasons = seasons;
+                  await tvShow.save();
+                  console.log("tvshow updated - new episode");
+                  countUpdatedTvShow++;
+                  continue;
                 }
               }
               // TODO: log here which tvshow has been update with name and data
@@ -166,60 +166,60 @@ export default class DiscoverController {
       }
 
       while (subDirectories.length > 0) {
-        const subDirectory = subDirectories.shift()
-        await goThrough(subDirectory.content, subDirectory.directory)
+        const subDirectory = subDirectories.shift();
+        await goThrough(subDirectory.content, subDirectory.directory);
       }
-    }
-    await goThrough(files)
+    };
+    await goThrough(files);
 
-    console.log("Ending discovering")
+    console.log("Ending discovering");
     res.json({
       countCreatedVideo,
       countCreatedTvShow,
       countUpdatedTvShow,
-    })
+    });
   }
 
   async discoverSubtitles(_, res) {
-    let countSubtitleCreated = 0
-    const subDirectories = []
-    const files = await findFiles(basePath)
+    let countSubtitleCreated = 0;
+    const subDirectories = [];
+    const files = await findFiles(basePath);
 
     const goThrough = async (files, extraPath = "") => {
       for (const file of files) {
-        const ext = path.extname(file.name)
-        const isSubtitle = regexIsSubtitle.test(ext)
+        const ext = path.extname(file.name);
+        const isSubtitle = regexIsSubtitle.test(ext);
 
-        const exclude = excludedExtension.some(ext => file.name.includes(ext))
+        const exclude = excludedExtension.some(ext => file.name.includes(ext));
 
-        if (exclude) continue
+        if (exclude) continue;
 
         if (file.isDirectory() || file.isSymbolicLink()) {
           //          console.log({file}, 'is directory')
-          const sub = await findFiles(basePath + "/" + extraPath + "/" + file.name)
+          const sub = await findFiles(basePath + "/" + extraPath + "/" + file.name);
           subDirectories.push({
             directory: extraPath + "/" + file.name,
             content: sub.filter(el => !el.name.includes("DS_Store")),
-          })
-          continue
+          });
+          continue;
         }
 
         // Here we need to work the file
-        const filename = path.basename(file.name, ext)
-        const absolutePath = path.resolve(basePath + "/" + extraPath + "/")
+        const filename = path.basename(file.name, ext);
+        const absolutePath = path.resolve(basePath + "/" + extraPath + "/");
 
         if (isSubtitle) {
           if (ext.includes("srt")) {
             createReadStream(absolutePath + "/" + file.name)
               .pipe(srt2vtt())
-              .pipe(createWriteStream(`${absolutePath}/${filename}.vtt`))
+              .pipe(createWriteStream(`${absolutePath}/${filename}.vtt`));
 
             // need to remove srt file
-            console.log("===========")
+            console.log("===========");
             // TODO: log here to keep track of what we delete
-            console.log("file to remove", absolutePath + "/" + file.name)
-            console.log("===========")
-            rm(absolutePath + "/" + file.name, () => {}) // trick to avoid error
+            console.log("file to remove", absolutePath + "/" + file.name);
+            console.log("===========");
+            // rm(absolutePath + "/" + file.name, () => {}); // trick to avoid error
           }
           subtitleModel.create(
             {
@@ -229,26 +229,26 @@ export default class DiscoverController {
             },
             (err, data) => {
               if (err) {
-                console.error(err)
+                console.error(err);
               }
-              console.log("success", data)
+              console.log("success", data);
             }
-          )
-          countSubtitleCreated++
-          continue
+          );
+          countSubtitleCreated++;
+          continue;
         }
       }
 
       while (subDirectories.length > 0) {
-        const subDirectory = subDirectories.shift()
-        await goThrough(subDirectory.content, subDirectory.directory)
+        const subDirectory = subDirectories.shift();
+        await goThrough(subDirectory.content, subDirectory.directory);
       }
-    }
-    await goThrough(files)
+    };
+    await goThrough(files);
 
     res.json({
       countSubtitleCreated,
-    })
+    });
   }
 
   discoverDetails() {
