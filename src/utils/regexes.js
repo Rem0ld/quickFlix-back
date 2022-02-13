b0239fdde031a15352934e5066ef9ff10bb62dd0
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-02-13 12:13:53 +0100

    update regex

--- src/utils/regexes.js
+++ src/utils/regexes.js
@@ -1,6 +1,8 @@
 export const matchExt = /(\w+)$/i;
 export const matchLng = /(\w+).\w+$/;
-export const regexTvShow = /s(\d{1,2})e(\d{1,2})|(\d{1,})x(\d{1,})/i;
+// Look for s01e01 | 1x01 | 101 - Removes number x264, h264 etc and quality 720p 1080p etc
+export const regexTvShow =
+  /s(\d{1,2})e(\d{1,2})|(\d{1,})x(\d{1,})|(?<!x|h|\d)(\d{1})(\d{2})(?!p|\d)/i;
 export const regExBasename =
   /([ .\w']+?)(Season|$|-|mp3|[s|S]\d{1,}|\(.*\)|[A-Z]{2,}|\W\d{4}\W?.*)/;
 export const regexIsSubtitle = /(vtt|srt|sfv)/i;
