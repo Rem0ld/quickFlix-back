f19c502091050af1c3601b054c533d93ca31aea8
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-02-13 12:14:06 +0100

    fn to download image

--- src/utils/fileManipulation.js
+++ src/utils/fileManipulation.js
@@ -1,4 +1,5 @@
-import { accessSync, constants, readdirSync } from "fs"
+import { accessSync, constants, createWriteStream, readdirSync } from "fs";
+import client from "https";
 
 export function subtitleExists(subPath) {
   // const extension = ['.srt', '.vtt']
@@ -7,14 +8,30 @@ export function subtitleExists(subPath) {
   //   fs.access(path + element, mode, callback)
   // })
   try {
-    accessSync(subPath + ".vtt", constants.R_OK)
+    accessSync(subPath + ".vtt", constants.R_OK);
   } catch (error) {
-    return false
+    return false;
   }
 
-  return true
+  return true;
 }
 
 export async function findFiles(dir) {
-  return readdirSync(dir, { withFileTypes: true })
+  return readdirSync(dir, { withFileTypes: true });
+}
+
+export async function downloadImage(url, filepath) {
+  return new Promise((resolve, reject) => {
+    client.get(url, res => {
+      if (res.statusCode === 200) {
+        res
+          .pipe(createWriteStream(filepath))
+          .on("error", reject)
+          .once("close", () => resolve(filepath));
+      } else {
+        res.resume();
+        reject(new Error(`Request failed with status code ${res.statusCode}`));
+      }
+    });
+  });
 }
