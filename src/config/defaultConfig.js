2de685e35c95113aa394d4f384cbca87b2a6a63c
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-02-14 14:12:52 +0100

    add config and regex

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,1 +1,15 @@
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const defaultLimit = 20;
+
+export const movieDbUrl = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.API_MOVIEDB}&page=1&include_adult=false&language=en-US&query=`;
+export const imageDbUrl = `https://image.tmdb.org/t/p/w500`;
+
+// Need to add id after tv/ then add the remaining of the URL
+export const detailDbUrl = `https://api.themoviedb.org/3`; // tv | movie / {id}
+export const apikeyUrl = `?api_key=${process.env.API_MOVIEDB}&language=en-US`;
+
+export const basePath = path.resolve("./public");
--- src/utils/regexes.js
+++ src/utils/regexes.js
@@ -3,7 +3,9 @@ export const matchLng = /(\w+).\w+$/;
 // Look for s01e01 | 1x01 | 101 - Removes number x264, h264 etc and quality 720p 1080p etc
 export const regexTvShow =
   /s(\d{1,2})e(\d{1,2})|(\d{1,})x(\d{1,})|(?<!x|h|\d)(\d{1})(\d{2})(?!p|\d)/i;
+// Adding I, to get Star wars movie
 export const regExBasename =
-  /([ .\w']+?)(Season|$|-|mp3|[s|S]\d{1,}|\(.*\)|[A-Z]{2,}|\W\d{4}\W?.*)/;
-export const regexIsSubtitle = /(vtt|srt|sfv)/i;
+  /([ .,\w']+?)(I,|Season|$|-|mp3|[s|S]\d{1,}|\(.*\)|[A-Z]{2,}|\W\d{4}\W?.*)/;
+export const regexIsSubtitle = /vtt|srt|sfv/i;
+export const regexVideo = /avi|mkv|mp4/;
 export const regexYearDate = /\d{4}(?!p)/;
