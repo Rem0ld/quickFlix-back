fc6b3a1008b2cc7c1769a91dfe79e02466216862
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-02-17 10:12:54 +0100

    polyfill replaceall

--- src/utils/stringManipulation.ts
+++ src/utils/stringManipulation.ts
@@ -1,7 +1,22 @@
-export const parseBasename = (s: string) => {
-  let r = s.match(/\./g);
-  if (result) {
 
-  }
-  return basename.replaceAll(".", " ").trim().toLowerCase();
+/**
+ * Simple version of replaceAll, as TS doesn't let me use it!!!
+ * @param target string 
+ * @param s string what we wanna change
+ * @param repl string replacement string
+ * @returns string the changed string
+ */
+export function replaceAll(target: string, s: string, repl: string): string {
+  return target.split(s).join(repl)
+}
+
+/**
+ * Modify he video name replacing points by spaces, trim and lower case the string
+ * @param s string
+ * @returns new string
+ */
+export const parseBasename = (s: string) => {
+  return replaceAll(s, ".", " ").trim().toLowerCase();
 };
+
+
