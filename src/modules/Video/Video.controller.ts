8adb07e7a975565dc83f68dad0ae9d86565f464e
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-04-12 14:35:59 +0200

    idem forgot join ""

--- src/modules/Video/Video.controller.ts
+++ src/modules/Video/Video.controller.ts
@@ -105,7 +105,7 @@ export default class VideoController {
 
     const promises = videos.map(video => {
       const basename = path.basename(video.filename)
-      const filename = basename.split(".").slice(0, basename.split(".").length - 1)
+      const filename = basename.split(".").slice(0, basename.split(".").length - 1).join(".")
       return videoModel.findByIdAndUpdate(video._id, {
         location: path.dirname(video.location),
         filename: `${filename}${video.ext}`
