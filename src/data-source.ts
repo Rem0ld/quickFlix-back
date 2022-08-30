c43d5076000ba1af0a3ec9bdb50b6301b3748b82
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-08-30 16:04:13 +0200

    add db secret to env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,4 +1,4 @@
-import "reflect-metadata";
+import dotenv from "dotenv";
 import { DataSource } from "typeorm";
 import { MovieDbJob } from "./modules/MovieDbJob/MovieDbJob.entity";
 import { TvShow } from "./modules/TvShow/TvShow.entity";
@@ -6,14 +6,15 @@ import { User } from "./modules/User/User.entity";
 import { Video } from "./modules/Video/Video.entity";
 import { Watched } from "./modules/Watched/Watched.entity";
 import { WatchedTvShow } from "./modules/WatchedTvShow/WatchedTvShow.entity";
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "0.0.0.0",
   port: 5432,
-  username: "docker",
-  password: "example",
-  database: "quickflix_db",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
   dropSchema: false,
