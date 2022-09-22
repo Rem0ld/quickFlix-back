9417f1c9981e69221ee279002d3eccb6051a03b9
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-09-22 16:44:59 +0200

    fix test class video

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -7,6 +7,8 @@ import VideoRepository from "../modules/Video/Video.repository";
 import VideoService from "../modules/Video/Video.service";
 import { TvShowRepository } from "../modules/TvShow/TvShow.repository";
 import TvShowService from "../modules/TvShow/TvShow.service";
+import { MovieDbJobRepository } from "../modules/MovieDbJob/MovieDbJob.repository";
+import MovieDbJobService from "../modules/MovieDbJob/MovieDbJob.service";
 dotenv.config();
 
 /**
@@ -26,8 +28,11 @@ export function configJest() {
 const userRepo = new UserRepository(AppDataSource.manager);
 export const userService = new UserService(userRepo);
 
+const movieJobRepo = new MovieDbJobRepository(AppDataSource.manager);
+const movieJobService = new MovieDbJobService(movieJobRepo);
+
 const videoRepo = new VideoRepository(AppDataSource.manager);
-export const videoService = new VideoService(videoRepo);
+export const videoService = new VideoService(videoRepo, movieJobService);
 
 const tvShowRepo = new TvShowRepository(AppDataSource.manager);
 export const tvShowService = new TvShowService(tvShowRepo);
